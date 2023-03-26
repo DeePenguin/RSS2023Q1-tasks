c5c9e2ed6bb59dd5d64db654993ada0072e782f9
DeePenguin <alivemushroom@gmail.com>
2023-03-26 17:40:23 +0400

    feat: add self-evaluation script

---
+++ shelter/scripts/index.js
@@ -0,0 +1,18 @@
+console.group('Страница Main (60)');
+  console.log('Проверка верстки +7');
+  console.log('Вёрстка соответствует макету +35');
+  console.log('Требования к css +6');
+  console.log('Интерактивность элементов +12');
+console.groupEnd('Страница Main (60)');
+
+console.group('Страница Pets (40)');
+  console.log('Проверка верстки +7');
+  console.log('Вёрстка соответствует макету +15');
+  console.log('Требования к css +4');
+  console.log('Интерактивность элементов +14');
+console.groupEnd('Страница Pets (40)');
+
+console.log(
+  '%cВсего: 100 / 100',
+  'font-weight: 600; font-size: 18px;'
+);
