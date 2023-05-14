bd6c05f6f694e7634d5a1b8ff55d7dec6fab47d7
DeePenguin <alivemushroom@gmail.com>
2023-05-15 03:50:51 +0400

    init: add basic configs & structure

---
+++ minesweeper/src/utils/base-component.js
@@ -0,0 +1,32 @@
+export default class BaseComponent {
+  constructor({
+    parentNode = null,
+    tag = 'div',
+    className = '',
+    content = '',
+    attributes = {},
+  }) {
+    const node = document.createElement(tag);
+    node.className = className;
+    node.innerHTML = content;
+    Object.entries(attributes).forEach(([prop, value]) => node.setAttribute(prop, value));
+    if (parentNode) parentNode.append(node);
+    this.node = node;
+  }
+
+  remove() {
+    this.node.remove();
+  }
+
+  appendTo(parentNode) {
+    parentNode.append(this.node);
+  }
+
+  append(...nodes) {
+    this.node.append(...nodes);
+  }
+
+  addListener(eventName, callback) {
+    this.node.addEventListener(eventName, callback);
+  }
+}
