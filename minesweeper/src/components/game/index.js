88f0bd69f8adc4e783218bdc8be1525fec842c03
DeePenguin <alivemushroom@gmail.com>
2023-05-17 09:14:34 +0400

    feat: add new game button

--- minesweeper/src/components/game/index.js
+++ minesweeper/src/components/game/index.js
@@ -2,31 +2,40 @@ import BaseComponent from '../../utils/base-component';
 import Field from '../field';
 
 const defaultSettings = {
-  size: 10,
-  bombsAmount: 10,
+  rows: 10,
+  cols: 10,
+  bombs: 10,
 };
 
 export default class Game extends BaseComponent {
   constructor(parentNode, settings = defaultSettings) {
     super({ parentNode, className: 'game' });
     this.settings = settings;
-    this.cellsToWin = this.settings.size ** 2 - this.settings.bombsAmount;
-    this.bombsLeft = this.settings.bombsAmount;
-    this.revealedCells = 0;
-    this.field = new Field(this, settings);
+    this.cellsToWin = this.settings.size ** 2 - this.settings.bombs;
+    this.field = new Field(this, this.settings.rows, this.settings.cols, this.settings.bombs);
     this.isActive = false;
-    this.moves = 0;
-    this.time = 0;
+    this.init();
     this.increaseTime = () => { this.time += 1; };
     this.on('startGame', () => this.start());
     this.on('lose', () => this.end());
-    this.on('clickOnCell', () => { this.moves += 1; });
+    this.on('move', () => { this.moves += 1; console.log('moves:', this.moves); });
     this.on('updateCellsCounter', (cellsNumber) => this.checkWin(cellsNumber));
     this.on('updateFlagsCounter', (isIncreased) => this.updateBombsLeftCounter(isIncreased));
+    this.on('newGame', () => this.newGame());
   }
 
   init() {
+    this.bombsLeft = this.settings.bombs;
+    this.revealedCells = 0;
+    this.moves = 0;
+    this.time = 0;
+  }
 
+  newGame() {
+    if (this.isActive) console.log('already active');
+    this.isActive = false;
+    this.field.reset();
+    this.init();
   }
 
   start() {
@@ -46,7 +55,7 @@ export default class Game extends BaseComponent {
   end() {
     this.pause();
     console.log('end');
-    console.log(this.moves);
+    console.log('moves:', this.moves);
   }
 
   checkWin(cellsNumber) {
