03dfadf063bd84c79babf59ab87fd2be244e4582
DeePenguin <alivemushroom@gmail.com>
2023-06-10 20:30:32 +0400

    refactor: change url to proxi

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import { Loader } from './loader';
 
 export class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://rss-news-api.onrender.com/', {
       apiKey: 'a110b858782643e69538761c75334b8c',
     });
   }
