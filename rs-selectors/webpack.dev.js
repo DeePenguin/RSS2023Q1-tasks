7aa2c313c3a45ec17d72da07820aad5a094e6adb
DeePenguin <alivemushroom@gmail.com>
2023-06-16 23:42:20 +0400

    feat: add configs & template

---
+++ rs-selectors/webpack.dev.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    static: { directory: path.resolve('dist') },
+    open: true,
+    host: 'localhost',
+    port: 9000,
+    hot: true,
+    compress: true,
+  },
+});
