b5b3f8e4bafcbd97e507a03cb0e86f8295314227
DeePenguin <alivemushroom@gmail.com>
2023-06-21 06:23:14 +0400

    feat: add page layout

---
+++ rs-selectors/src/app/components/layout/header/header.ts
@@ -0,0 +1,6 @@
+import { BaseComponent } from '../../../../utils/base-component'
+import './header.scss'
+
+const content = '<h1 class = "header__title">RS-Selectors</h1>'
+
+export const header = new BaseComponent({ tag: 'header', className: 'header', content })
---
+++ rs-selectors/src/app/components/layout/panel/panel.ts
@@ -0,0 +1,6 @@
+import { BaseComponent } from '../../../../utils/base-component'
+import './panel.scss'
+
+const content = 'Panel'
+
+export const panel = new BaseComponent({ tag: 'section', className: 'panel', content })
