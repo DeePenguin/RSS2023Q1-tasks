fff9f2174f7a2c7dfefaf2b871a79490d6d486d1
DeePenguin <alivemushroom@gmail.com>
2023-06-21 04:45:22 +0400

    chore: update config

--- rs-selectors/webpack.common.js
+++ rs-selectors/webpack.common.js
@@ -8,7 +8,7 @@ const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const PROJECT = {
   title: 'RS Selectors',
   lang: 'en',
-  isEmptyBodyRequired: false,
+  isEmptyBodyRequired: true,
 }
 
 const getTemplate = (subtitle) => `<!DOCTYPE html>
@@ -23,6 +23,8 @@ const getTemplate = (subtitle) => `<!DOCTYPE html>
   </body>
 </html>`
 
+const filename = (mode, ext) => `${mode === 'production' ? '[contenthash]' : '[name]'}.${ext}`
+
 module.exports = {
   entry: {
     app: path.resolve(__dirname, './src/index.ts'),
@@ -30,7 +32,7 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
+    filename: filename(this.mode, 'js'),
     clean: true,
     assetModuleFilename: 'assets/[name][ext]',
   },
@@ -46,26 +48,48 @@ module.exports = {
   },
 
   plugins: [
-    new StylelintPlugin({ extensions: ['scss'] }),
-    new ESLintPlugin({ extensions: ['ts'] }),
+    new StylelintPlugin({
+      extensions: ['scss'],
+      fix: true,
+    }),
+    new ESLintPlugin({
+      extensions: ['ts']
+    }),
     new HtmlWebpackPlugin({
       templateContent: getTemplate(),
       fileName: 'index.html',
       chunks: ['app'],
     }),
-    new MiniCssExtractPlugin({ filename: '[contenthash].css' }),
+    new MiniCssExtractPlugin({
+      filename: filename(this.mode, 'css'),
+    }),
     new FaviconsWebpackPlugin(path.resolve(__dirname, 'src/assets/favicon.png'))
   ],
 
   module: {
-    rules: [
-      { test: /\.ts?$/i, use: ['ts-loader'] },
-      { test: /\.(c|sa|sc)ss$/i,
-        use: [this.mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+    rules: [{
+        test: /\.ts?$/i,
+        use: ['ts-loader']
       },
-      { test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp|mp3)$/i, type: 'asset' },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [this.mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: ['src/styles/variables.scss', 'src/styles/mixins.scss'],
+            },
+          },
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp|mp3)$/i,
+        type: 'asset'
+      },
     ],
   },
 
-  resolve: { extensions: ['.ts', '.js', '...'] },
+  resolve: {
+    extensions: ['.ts', '.js', '...']
+  },
 }
