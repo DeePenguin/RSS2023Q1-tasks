5b83a01de94ee8cdfe565bacdf84ab243a4f3ae0
DeePenguin <alivemushroom@gmail.com>
2023-06-28 21:23:53 +0400

    feat: add css code highlight

--- rs-selectors/src/app/components/game/editor/css-editor/css-editor.ts
+++ rs-selectors/src/app/components/game/editor/css-editor/css-editor.ts
@@ -1,3 +1,4 @@
+import css from 'highlight.js/lib/languages/css'
 import { Editor } from '../editor'
 import { BaseComponent } from '../../../../../utils/base-component'
 import { CssEditorParams } from '../../../../../utils/constants'
@@ -5,10 +6,19 @@ import { CssEditorParams } from '../../../../../utils/constants'
 import './css-editor.scss'
 
 export class CssEditor extends Editor {
+  private answer = new BaseComponent({
+    className: 'editor__answer',
+  })
   private input = new BaseComponent({
-    tag: 'input',
-    className: 'editor__input',
-    attr: { type: 'text', placeholder: CssEditorParams.placeholder },
+    parent: this.answer,
+    tag: 'textarea',
+    className: 'answer__input',
+    attr: { rows: '1', placeholder: CssEditorParams.placeholder },
+  })
+  private markup = new BaseComponent({
+    parent: this.answer,
+    className: 'answer__markup language-css',
+    attr: { 'data-placeholder': CssEditorParams.placeholder },
   })
 
   private button = new BaseComponent({
@@ -23,25 +33,52 @@ export class CssEditor extends Editor {
     this.addClass('editor-css')
     this.title.setContent(CssEditorParams.title)
     this.fileName.setContent(CssEditorParams.fileName)
-    this.content.append(this.input, this.button)
+    this.content.append(this.answer, this.button)
+    this.hljs.registerLanguage('css', css)
+    this.input.addListener('keydown', (e: KeyboardEvent): void => this.handleKeydown(e))
+    this.input.addListener('input', (): void => this.handleInput())
+    this.button.addListener('click', (): void => this.emitCheck())
   }
 
-  public type(answer: string): void {
-    this.input.node.value = ''
-    this.input.node.disabled = true
-    let i = 0
-    setTimeout(() => {
-      const printChar = (): void => {
-        if (i === answer.length) {
-          this.input.node.disabled = false
-          return
-        }
+  private handleKeydown(e: KeyboardEvent): void {
+    if (e.code === 'Enter') {
+      e.preventDefault()
+      this.emitCheck()
+    }
+  }
 
-        this.input.node.value += answer[i]
-        i += 1
-        setTimeout(printChar, 300)
-      }
-      printChar()
-    }, 500)
+  private handleInput(): void {
+    this.highlight()
+  }
+
+  private highlight(): void {
+    this.markup.node.innerHTML = this.input.node.value
+    this.hljs.highlightElement(this.markup.node)
+  }
+
+  private emitCheck(): void {
+    console.log('emit answer check', this.input.node.value)
+  }
+
+  public shake() :void {
+    this.addClass('shake')
+    this.addListener('animationend', () => this.removeClass('shake'))
+  }
+  public type(answer: string): void {
+      this.input.node.value = ''
+      this.input.node.disabled = true
+      let i = 0
+      setTimeout(() => {
+        const typeChar = (): void => {
+          if (i === answer.length) {
+            this.input.node.disabled = false
+            return
+          }
+          this.input.node.value += answer[i]
+          i += 1
+          setTimeout(typeChar, 200)
+        }
+        typeChar()
+      }, 500)
   }
 }
