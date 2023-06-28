f429d71cc5773791a77ce40676a8eb2be295417e
DeePenguin <alivemushroom@gmail.com>
2023-06-29 00:55:16 +0400

    feat: add right answer check

--- rs-selectors/src/app/components/game/viewer/viewer.ts
+++ rs-selectors/src/app/components/game/viewer/viewer.ts
@@ -7,6 +7,7 @@ import { EventEmitter } from '../../../../utils/event-emitter'
 
 export class Viewer extends BaseComponent {
   public elements: HTMLElement[] = []
+  private selectedElements: HTMLElement[] = []
   constructor(private emitter: EventEmitter) {
     super({ className: 'game__viewer' })
   }
@@ -37,5 +38,21 @@ export class Viewer extends BaseComponent {
       root.append(node)
       this.elements.push(node)
     })
+  }
+
+  public applySelector(selector: string): number {
+    this.selectedElements = Array.from(this.node.querySelectorAll(selector))
+    const matchedElements = this.selectedElements.filter((el) => el.classList.contains('animated'))
+    return matchedElements.length
+  }
+
+  public completeLevel(): void {
+    this.addClass('finish-level')
+    const finishListener = (): void => {
+      this.node.removeEventListener('animationend', finishListener)
+      this.removeClass('finish-level')
+      this.emitter.emit('completeAnimationEnds', null)
+    }
+    this.addListener('animationend', finishListener)
   }
 }
