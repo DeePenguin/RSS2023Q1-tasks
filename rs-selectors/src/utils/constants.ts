5e18493a8e864918493880f1373961c179747b03
DeePenguin <alivemushroom@gmail.com>
2023-06-28 21:23:53 +0400

    feat: add css code highlight

--- rs-selectors/src/utils/constants.ts
+++ rs-selectors/src/utils/constants.ts
@@ -11,7 +11,7 @@ export const CssEditorParams = {
 export const HtmlEditorParams = {
   fileName: 'table.html',
   title: 'HTML Viewer',
-  minLinesAmount: 15,
+  minLinesAmount: 13,
 }
 
 export const defaultGameData: GameData = {
