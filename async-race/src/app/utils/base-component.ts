f44b29bad0297f876f7bdfb900f2ee0ea44405ff
DeePenguin <alivemushroom@gmail.com>
2023-07-14 20:50:16 +0400

    feat: add utils

---
+++ async-race/src/app/utils/base-component.ts
@@ -0,0 +1,79 @@
+import type { Component } from '@/app/shared/models/component.model'
+
+type ComponentProps<T> = {
+  parent?: HTMLElement | Component
+  tag: T
+  className: string
+  content: string
+  attr: Record<string, string>
+}
+
+export class BaseComponent<T extends keyof HTMLElementTagNameMap = 'div'> implements Component {
+  protected node: HTMLElementTagNameMap[T]
+  constructor({ parent, tag = 'div' as T, className = '', content = '', attr }: Partial<ComponentProps<T>>) {
+    this.node = document.createElement(tag)
+    this.node.className = className
+    this.node.innerHTML = content
+    if (attr) {
+      this.setAttributes(attr)
+    }
+    if (parent) {
+      parent.append(this.node)
+    }
+  }
+
+  public getNode(): HTMLElementTagNameMap[T] {
+    return this.node
+  }
+
+  public remove(): void {
+    this.node.remove()
+  }
+
+  public appendTo(parent: HTMLElement | Component): void {
+    parent.append(this.node)
+  }
+
+  public append<K extends keyof HTMLElementTagNameMap>(
+    ...components: (HTMLElement | BaseComponent<K> | string)[]
+  ): void {
+    this.node.append(
+      ...components.map((component) => (component instanceof BaseComponent ? component.node : component)),
+    )
+  }
+
+  public addListener<E extends keyof HTMLElementEventMap>(
+    eventName: E,
+    callback: (ev: HTMLElementEventMap[E]) => void,
+  ): void {
+    this.node.addEventListener(eventName, callback as EventListenerOrEventListenerObject)
+  }
+
+  public setAttributes(attributes: Record<string, string>): void {
+    Object.entries(attributes).forEach(([prop, value]) => this.node.setAttribute(prop, value))
+  }
+
+  public removeAttributes(...attributes: string[]): void {
+    attributes.forEach((attribute) => this.node.removeAttribute(attribute))
+  }
+
+  public setContent(content: string): void {
+    this.node.textContent = content
+  }
+
+  public addClass(...classNames: string[]): void {
+    this.node.classList.add(...classNames)
+  }
+
+  public removeClass(...classNames: string[]): void {
+    this.node.classList.remove(...classNames)
+  }
+
+  public toggleClass(className: string, state?: boolean): void {
+    this.node.classList.toggle(className, state)
+  }
+
+  public setStyle(props: { [keys: string]: string }): void {
+    Object.assign(this.node.style, props)
+  }
+}
