1c0b6c9ee126d4816dc83e1a84e82102393cf905
DeePenguin <alivemushroom@gmail.com>
2023-07-14 20:50:16 +0400

    feat: add utils

---
+++ async-race/src/app/shared/models/component.model.ts
@@ -0,0 +1,13 @@
+export interface Component {
+  getNode(): HTMLElementTagNameMap[keyof HTMLElementTagNameMap]
+  remove(): void
+  appendTo(parent: HTMLElement | Component): void
+  append(...components: (HTMLElement | Component | string)[]): void
+  addListener(eventName: keyof GlobalEventHandlersEventMap, callback: () => void): void
+  setAttributes(attributes: Record<string, string>): void
+  removeAttributes(...attributes: string[]): void
+  setContent(content: string): void
+  addClass(...classNames: string[]): void
+  removeClass(...classNames: string[]): void
+  toggleClass(className: string, state?: boolean): void
+}
