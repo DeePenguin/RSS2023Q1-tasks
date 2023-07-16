97237ee5d045a4df4f4636d3ac6429f1bd5ecbc2
DeePenguin <alivemushroom@gmail.com>
2023-07-16 04:59:09 +0400

    feat: add http service

--- async-race/src/app/core/types/types.ts
+++ async-race/src/app/core/types/types.ts
@@ -2,3 +2,10 @@ import type { Component } from '@core/models/component.model'
 
 export type PagesRecord = Record<string, Component>
 export type RoutesMap = Map<string, () => void>
+export type RequestOptions = { body?: string; headers?: Record<string, string> }
+export type RequestProps = {
+  url?: number | string
+  query?: Record<string, string | number>
+  body?: Record<string, string>
+  headers?: Record<string, string>
+}
