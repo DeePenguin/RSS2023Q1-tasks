eeb6bda8d48fc6609754fc5d8d68f25116c2d13a
DeePenguin <alivemushroom@gmail.com>
2023-07-22 02:24:19 +0400

    feat: add garage list

---
+++ async-race/src/app/core/models/events-map.model.ts
@@ -0,0 +1,4 @@
+export type EventsMap = {
+  'update-car': (id: number) => void
+  'delete-car': (id: number) => void
+}
--- async-race/src/app/garage/garage.ts
+++ async-race/src/app/garage/garage.ts
@@ -3,14 +3,18 @@ import { PageHeader } from '@shared/components/page-header/page-header'
 import { Pagination } from '@shared/components/pagination/pagination'
 import { BaseComponent } from '@utils/base-component'
 import type { PageState } from '@core/types/types'
+import { EventEmitter } from '@utils/event-emitter'
+import type { EventsMap } from '@core/models/events-map.model'
 import { GarageService } from './services/garage.service'
+import { GarageList } from './components/garage-list/garage-list'
 
 export class Garage extends BaseComponent<'section'> {
   private itemsPerPage = pageLimits.garage
+  private emitter = new EventEmitter<EventsMap>()
   private garageService: GarageService
   private header: PageHeader
   private pagination: Pagination
-  private carsContainer = new BaseComponent({ className: 'garage__container' })
+  private list = new GarageList(this.emitter)
 
   constructor(private store: PageState) {
     super({ tag: 'section', className: 'garage' })
@@ -23,14 +27,15 @@ export class Garage extends BaseComponent<'section'> {
       () => this.previousPage(),
       () => this.nextPage(),
     )
-    this.append(this.header, this.pagination, this.carsContainer)
+    this.append(this.header, this.pagination, this.list)
     this.renderPage()
   }
 
   private renderPage(): void {
     const cars = this.garageService.getCars(this.store.currentPage)
     cars
-      .then((carsData) => this.carsContainer.setContent(JSON.stringify(carsData)))
+      // .then((carsData) => this.carsContainer.setContent(JSON.stringify(carsData)))
+      .then((carsData) => this.list.showCars(carsData))
       .catch((err) => {
         console.error(err)
       })
